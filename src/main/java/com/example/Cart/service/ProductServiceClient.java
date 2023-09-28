61727dcb716f7ddb84e41a786b3a1beba9453d37
Nguyen Van Vu <vu27082002@gamil.com>
2023-09-28 19:10:06 +0700

    7:05 pm 28/9

--- src/main/java/com/example/Cart/service/ProductServiceClient.java
+++ src/main/java/com/example/Cart/service/ProductServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "Products", url = "http://localhost:8991")
+@FeignClient(name = "Products", url = "http://localhost:8991/Products")
 public interface ProductServiceClient {
         @GetMapping("/api/v1/products/{id}")
         ResponseEntity<Product> getProductById(@PathVariable("id") Long id);
