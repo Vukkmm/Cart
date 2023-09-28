055e1569c91336c3400873615922897a350f5e58
Nguyen Van Vu <vu27082002@gamil.com>
2023-09-28 19:06:07 +0700

    7:05 pm 28/9

--- src/main/java/com/example/Cart/CartApplication.java
+++ src/main/java/com/example/Cart/CartApplication.java
@@ -2,8 +2,12 @@ package com.example.Cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+//Sau khi bạn đã kích hoạt @EnableFeignClients, bạn có thể tiến hành sử dụng các Feign Client
+//được tạo ra trong ứng dụng của bạn để giao tiếp với các microservices khác trong mô hình kiến trúc microservices.
 public class CartApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +15,4 @@ public class CartApplication {
 	}
 
 }
+
--- src/main/java/com/example/Cart/service/ProductServiceClient.java
+++ src/main/java/com/example/Cart/service/ProductServiceClient.java
@@ -1,5 +1,67 @@
-package com.example.Cart.service;
+    package com.example.Cart.service;
 
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "Products", url = "http://localhost:8991")
 public interface ProductServiceClient {
+        @GetMapping("/api/v1/products/{id}")
+        ResponseEntity<Product> getProductById(@PathVariable("id") Long id);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
