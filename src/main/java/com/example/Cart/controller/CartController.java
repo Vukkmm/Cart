b5485b834e665cfc9bca6daeb64e42c3155a77f0
Nguyen Van Vu <vu27082002@gamil.com>
2023-10-02 10:05:51 +0700

    10:04 am 10/2

--- src/main/java/com/example/Cart/controller/CartController.java
+++ src/main/java/com/example/Cart/controller/CartController.java
@@ -1,21 +1,33 @@
 package com.example.Cart.controller;
 
 import com.example.Cart.service.CartService;
+import com.example.Cart.service.ProductServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/carts")
 public class CartController {
     @Autowired
     CartService cartService;
+
     @PostMapping
     public ResponseEntity<String> addToCart(@PathVariable Long userId, @PathVariable Long productId) {
         cartService.addToCart(userId, productId);
         return ResponseEntity.ok("Product da duoc them thanh cong");
     }
+
+    private final ProductServiceClient productServiceClient;
+
+    @Autowired
+    public CartController(ProductServiceClient productServiceClient) {
+        this.productServiceClient = productServiceClient;
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getProduct(@PathVariable Long id) {
+        return ResponseEntity.ok().body(productServiceClient.getProductById(id));
+    }
+
 }
